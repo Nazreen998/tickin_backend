d0423a14e0c12a19ca05e142643664491c88f3e0
Nazreen998 <nareenbegam1998@gmail.com>
2026-01-04 15:24:28 +0530

    Updated

--- src/routes/vehicles.routes.js
+++ src/routes/vehicles.routes.js
@@ -1,7 +1,7 @@
 import express from "express";
 import { verifyToken } from "../middleware/auth.middleware.js";
 import { allowRoles } from "../middleware/role.middleware.js";
-import { getAvailableVehicles } from "./vehicles.service.js";
+import { getAvailableVehicles, addVehicle } from "./vehicles.service.js";
 
 const router = express.Router();
 
@@ -12,5 +12,12 @@ router.get(
   allowRoles("MANAGER", "MASTER"),
   getAvailableVehicles
 );
+router.post(
+  "/add",
+  verifyToken,
+  allowRoles("MANAGER"),
+  addVehicle
+);
+
 
 export default router;
