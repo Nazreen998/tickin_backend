2e756ca071e0f3f5b472dd7ddbfc731ad9dd81ec
Nazreen998 <nareenbegam1998@gmail.com>
2026-01-27 11:58:50 +0530

    Updated

--- src/modules/slot/slot.routes.js
+++ src/modules/slot/slot.routes.js
@@ -9,7 +9,7 @@ import {
   managerDisableSlot,
   cancelHalfMerge,
   confirmHalfMerge,
-
+  managerCancelConfirmedMerge,
   managerConfirmMerge,
   managerCancelConfirmedDayMerge,
   managerMoveBookingToMerge,
