cf375d927ee12e7312bb4e41611acb4e60c5ba20
Nazreen998 <nareenbegam1998@gmail.com>
2026-02-05 17:52:36 +0530

    QR CODE

--- server.js
+++ server.js
@@ -33,11 +33,10 @@ import attendanceRoutes from "./src/modules/attendance/attendance.routes.js";
 import ManagerOrdersFlowRoutes from "./src/routes/managerOrdersFlow.routes.js";
 import attendanceDashboardRoutes from "./src/modules/attendance/dashboard.routes.js";
 import attendanceConfigRoutes from "./src/modules/attendance/config.routes.js";
+const app = express();
 app.use(cors());
 app.use(express.json());          // ✅ THIS IS MUST
 app.use(express.urlencoded({ extended: true }));
-
-const app = express();
 app.use("/qr", qrRoutes);
 /**
  * ✅ Important for Render/Railway/Proxy hosting
