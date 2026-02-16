c222fd0e81e5a09cfdd1730aae5fd1a979357766
gokilavani-dev <gokilavani2k@gmail.com>
2026-02-16 20:59:04 +0530

    fixing that single slotconfirmed orders are not shown

--- src/modules/orders/orders.service.js
+++ src/modules/orders/orders.service.js
@@ -89,11 +89,6 @@ for (const b of bookings) {
 
   const flowKey = masterId || mk || oid;
 
-  /* ❌ skip FULL master booking itself */
-if (oid === flowKey) {
-  continue;
-}
-
   if (!grouped[flowKey]) {
     grouped[flowKey] = {
       flowKey,
